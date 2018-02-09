1bfb36c567a43cfd2c5c0d6bbe04953fadff3dfa
PadmajaVB <padmajabhagwat4@gmail.com>
2018-02-09 13:23:02 +0530

    another approach to make the model more generalizable

---
+++ AnotherApproach/main.py
@@ -0,0 +1,21 @@
+import keras
+import gensim
+
+#TODO preprocess the input file to get standard vectors 
+
+""" Model designing part """
+#TODO design encoder
+
+#TODO design multi-level aligner
+
+#TODO design decoder
+
+""" Training the model """
+#TODO write training code
+
+#TODO map the o/p action sequence to the i/p instruction for effective backpropagation 
+
+""" Testing the model """
+#TODO write testing code
+
+#TODO simulate the output
