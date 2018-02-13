2bb7e787256ba5ef16114cc4a7bc417775d54022
PadmajaVB <padmajabhagwat4@gmail.com>
2018-02-13 17:18:50 +0530

    adding code for multilevel-aligner

---
+++ AnotherApproach/multilevel_aligner.py
@@ -0,0 +1,20 @@
+import numpy as np
+import utils
+
+
+def generate_context_vector(input_vector, hidden_state_vector):
+	irows = input_vector.shape[1]
+	icols = input_vector.shape[0]  # TODO: replace this with timesteps
+	input_wt = utils.random_weights(irows, icols)
+
+	hrows = hidden_state_vector.shape[1]
+	hcols = hidden_state_vector.shape[0]
+	hidden_wt = utils.random_weights(hrows, hcols)
+
+	beta = np.tanh(np.dot(input_vector, input_wt) + np.dot(hidden_state_vector, hidden_wt))
+
+	alpha = utils.softmax(beta)
+
+	context_vector = np.multiply(alpha, np.concatenate((input_vector, hidden_state_vector), axis=1))
+
+	return context_vector
