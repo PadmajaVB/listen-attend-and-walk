79ff0dc495616d63372bc948e173ab906a5f628f
PadmajaVB <padmajabhagwat4@gmail.com>
2018-03-28 18:44:17 +0530

    modifying the main file

--- AnotherApproach/main.py
+++ AnotherApproach/main.py
@@ -1,21 +1,31 @@
-import keras
-import gensim
+import config
+import DataProcessing
+from gensim.models import Word2Vec
 
-#TODO preprocess the input file to get standard vectors 
 
-""" Model designing part """
-#TODO design encoder
+def main():
+	# TODO preprocess the input file to get standard vectors
+	configuration=config.get_config()
+	filepath = configuration['datafile_path']
+	word_embedding = configuration['word_embedding']
+	processed_data = DataProcessing.ProcessData(filepath)
+	word_vectors = Word2Vec.load(word_embedding)
+	print "Word embeddings: ", word_vectors
 
-#TODO design multi-level aligner
+	""" Model designing part """
+	# TODO design encoder
 
-#TODO design decoder
+
+# TODO design multi-level aligner
+
+# TODO design decoder
 
 """ Training the model """
-#TODO write training code
+# TODO write training code
 
-#TODO map the o/p action sequence to the i/p instruction for effective backpropagation 
+# TODO map the o/p action sequence to the i/p instruction for effective backpropagation
 
 """ Testing the model """
-#TODO write testing code
+# TODO write testing code
 
-#TODO simulate the output
+# TODO simulate the output
