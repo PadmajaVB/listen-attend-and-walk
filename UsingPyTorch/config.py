d9e69cf51d00a794577fe327839484a56591879b
Manisha <manishajhawar96@gmail.com>
2018-04-06 16:28:37 +0530

    Modified config file.

--- UsingPyTorch/config.py
+++ UsingPyTorch/config.py
@@ -4,12 +4,15 @@ def get_config():
 	input_params['map2'] = "jelly"
 	input_params['map3'] = "l"
 	input_params['maps_train'] = ["grid", "jelly"]
+	input_params['map_test'] = "l"
 	input_params['max_epochs'] = 2
 	input_params['seed'] = 12345
 	input_params['datafile_path'] = "./data/"
 	input_params['save_filepath'] = "./Tracks/"
 	input_params['word_embedding'] = "./data/WordEmbeddings.bin"
+
 	return input_params
+
 
 def get_model_config():
 	model_params = {}
@@ -21,5 +24,5 @@ def get_model_config():
 	model_params['dim_lang'] = 524
 	model_params['dim_world'] = 78  # pre-defined world representations : 6 + 4 * (8+3+6+1) = 78
 	model_params['dim_action'] = 4  # forward, left, right, stop
-	return model_params
 
+	return model_params
